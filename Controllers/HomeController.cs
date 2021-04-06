f4ce946ac2e5dab7f88d132f3a5a10155e08e038
Andrew Hejl <andrew.hejl@outlook.com>
2021-04-05 19:39:04 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClosedCircuit.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Scores()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Round.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClosedCircuit.Models
+{
+    public class Round
+    {
+        public int RoundNumber { get; set; }
+        public int SmallSpawn { get; set; }
+        public int FastSpawn { get; set; }
+        public int FatSpawn { get; set; }
+        public int BigSpawn { get; set; }
+
+    }
+}
